11bb2f4de02983e586c65629f1e9d91f02b79b6b
Maxwell Moazzam <max.moazzam@gmail.com>
2016-04-15 16:00:26 -0700

    Update similarity.js
    Documentation formatting

--- similarity.js
+++ similarity.js
@@ -4,27 +4,27 @@
 
 var similarity = function(array1, array2) {
 	
-	if (array1.length !== array2.length) {										//If arrays are of different lengths an error message is returned
+	if (array1.length !== array2.length) {						//If arrays are of different lengths an error message is returned
 		return 'Arrays are of different lengths.';
 	}
 	
-	if (array1.length == 0 || array2.length == 0) {								//If arrays have no values, then an error message is returned
+	if (array1.length == 0 || array2.length == 0) {					//If arrays have no values, then an error message is returned
 		return 'An array was not inputted.';
 	}
 
-	var same = 0;																//Variables for counting similar or different hits
+	var same = 0;									//Variables for counting similar or different hits
 	var different = 0;
 
-	for (i=0; i < array1.length; i++) {											//If a hit is made, the same variable is increased
+	for (i=0; i < array1.length; i++) {						//If a hit is made, the same variable is increased
 		if (array1[i] === array2[i]) {
 			same++;
 		}
-		else if (array1[i] !== array2[i]) {										//If the numbers are different, the different variable is increased
+		else if (array1[i] !== array2[i]) {					//If the numbers are different, the different variable is increased
 			different++;
 		}
 	}
 
-	var percentSame = ((same / array1.length) * 100).toPrecision(4);			//Percentages are calculated
+	var percentSame = ((same / array1.length) * 100).toPrecision(4);		//Percentages are calculated
 	var percentDifferent = ((different / array1.length) * 100).toPrecision(4);
 
 	return 'The two arrays had ' + same + ' similar hit(s), and ' + different + ' different hit(s). ' + percentSame + '% is similiar and ' + percentDifferent + '% is different.';
